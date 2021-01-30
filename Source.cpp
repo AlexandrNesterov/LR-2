33f6179dc473c06b19cdb9c1b8d4a8d3feb2b4f1
AlexandrNesterov <71148837+AlexandrNesterov@users.noreply.github.com>
2021-01-30 16:37:13 +0300

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -3,6 +3,7 @@
 int main() {
 	char str[100];
 	char word;
+	char word2;
 	char c;
 	int i = 0;
 	int start = 0;
@@ -15,15 +16,21 @@ int main() {
 	str[i] = ' ';
 	printf("Input word ");
 	scanf("%c", &word);
+	if (word > 64 && word < 91) {
+		word2 = word + 32;
+	}
+	if (word > 96 && word < 123) {
+		word2 = word - 32;
+	}
 	for (int k = 0; k <= i; k++) {
 
-		if ((str[k] == ' ' || str[k] == ',' || str[k] == '.' || str[k] == '!' || str[k] == '?' ) && str[k - 1] == word) {
+		if ((str[k] == ' ' || str[k] == ',' || str[k] == '.' || str[k] == '!' || str[k] == '?') && (str[k - 1] == word || str[k - 1] == word2)) {
 
 			finish = k;
 			for (int j = start; j < finish; j++) {
 				printf("%c", str[j]);
 			}
-		
+
 			printf("\n");
 		}
 		for (; str[k] == ' ' || str[k] == ',' || str[k] == '.' || str[k] == '!' || str[k] == '?'; k++) {
@@ -31,7 +38,8 @@ int main() {
 		}
 
 	}
-	
+
 	return 0;
 }
+
 
